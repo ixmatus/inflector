5b6bd50e3505800a121be1eb80d3e073ee34b969
Parnell Springmeyer <ixmatus@gmail.com>
2013-07-28 14:24:36 -0500

    Adding new files and updating setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,31 @@
 import sys
 from setuptools import setup, find_packages
 
-version = '2.0'
-
 if not '2.5' <= sys.version:
     raise ImportError('Python version not supported')
 
 tests_require = []
 
+with open("LICENSE", 'r') as f:
+    LICENSE = f.read()
+
+with open("README.md", 'r') as f:
+    README = f.read()
+
 setup(
     name="Inflector",
-    version=version,
+    version="2.0.2",
     description="A port of ROR's inflector class",
-    long_description="""\
-        This is a fork of bermi's original `inflector class <https://github.com/bermi/Python-Inflector>`_ - I
-        mostly wanted it up on PyPi and will be making some changes to it.
-        It has a `mercurial repository
-        <https://ixmatus@bitbucket.org/ixmatus/inflector>`_ that
-        you can install from with ``easy_install inflector``
-        """,
+    long_description=README,
     classifiers=["Intended Audience :: Developers",
         "License :: OSI Approved :: Python Software Foundation License",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
     author="Parnell Springmeyer",
-    author_email="ixmatus@gmail.com",
-    url="http://bitbucket.org/ixmatus/inflector",
-    license="BSD",
+    author_email="parnell@ixmat.us",
+    url="https://github.com/ixmatus/inflector",
+    license=LICENSE,
     zip_safe=False,
     packages=find_packages(),
     include_package_data=True,
