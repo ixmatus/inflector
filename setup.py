78739f8cb9df3e126bf13f6baa07b958651c4f75
Parnell Springmeyer <parnell@ixmat.us>
2014-08-14 14:59:47 -0500

    Fixing the setup.py so it actually installs the correct files

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
-import sys
+import os
+from setuptools import setup
 
-from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
 
-if not '2.5' <= sys.version:
-    raise ImportError('Python version not supported')
+README = open(os.path.join(here, 'README.md')).read()
+VERSION = open(os.path.join(here, 'VERSION')).read()
 
-tests_require = []
-
-with open("LICENSE", 'r') as f:
-    LICENSE = f.read()
-
-setup(
-    name="Inflector",
-    version="2.0.7",
-    description="A port of ROR's inflector class",
-    classifiers=["Intended Audience :: Developers",
-        "License :: OSI Approved :: Python Software Foundation License",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+setup(name='Inflector',
+      version=VERSION.strip(),
+      long_description=README,
+      classifiers=["Intended Audience :: Developers",
+                   "License :: OSI Approved :: Python Software Foundation License",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-    author="Parnell Springmeyer",
-    author_email="parnell@ixmat.us",
-    url="https://github.com/ixmatus/inflector",
-    license=LICENSE,
-    test_suite='tests',
-    data_files=[('', ["LICENSE", "README.md", "CHANGES", "DEBT"])],
-    packages=["inflector"]
-)
+      author='Parnell Springmeyer',
+      author_email="parnell@digitalmentat.com",
+      url="https://github.com/ixmatus/inflector",
+      keywords="inflector text language english",
+      packages=["inflector"],
+      package_dir={'inflector': 'inflector'},
+      data_files=[
+          ("", ['README.md', 'LICENSE', 'VERSION'])
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='tests'
+      )
+
