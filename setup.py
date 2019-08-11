e42605d552c91c40645517c2948459ccc8ebc184
Hubert Piotrowski <agaihubi@gmail.com>
2019-08-12 05:46:22 +0800

    Fixes for python 3 (#9)

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-README = open(os.path.join(here, 'README.md')).read()
-VERSION = open(os.path.join(here, 'VERSION')).read()
+README = open(os.path.join(here, 'README.md'), 'r', encoding='utf-8').read()
+VERSION = open(os.path.join(here, 'VERSION'), 'r', encoding='utf-8').read()
 
 setup(name='Inflector',
       version=VERSION.strip(),
